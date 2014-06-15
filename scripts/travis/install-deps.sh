bd2bf61c4c1570451553e81156542453f348ca7c
Tommy Back <tommy.back@iki.fi>
2014-06-15 21:53:59 +0200

    Initial CMake version for building.

--- scripts/travis/install-deps.sh
+++ scripts/travis/install-deps.sh
@@ -17,7 +17,7 @@ install_cppcheck()
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
 sudo apt-get -qq install g++-4.8
-sudo apt-get install scons
+sudo apt-get install cmake
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
 install_cppcheck
