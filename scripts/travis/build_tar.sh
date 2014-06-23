0c186c28e2407fedb2a68ed7168f66e152be9278
Tommy Back <tommy.back@iki.fi>
2014-06-23 21:50:04 +0200

    Build tar ball after successful Travis CI build.

---
+++ scripts/travis/build_tar.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+os="$1"
+if [[ -z "$1" ]] ; then
+    os="linux"
+fi
+
+dir="opentxs-server"
+mkdir ${dir}
+
+# collect files
+cp -rf build/bin/opentxs-auditor ${dir}
+
+# create package
+version=`cat VERSION`
+compiler=${CXX}
+package="opentxs-auditor-${version}-${os}-${compiler}.tar.gz"
+
+echo "Creating package ${package}"
+tar -vpczf ${package} ${dir}
+
+mkdir s3
+cp ${package} s3/
